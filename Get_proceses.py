e12ce128da36b6020bec6b8f2679dfa1148602da
geneus003 <geneus003@gmail.com>
2018-11-14 22:22:28 +0300

    Returns all active processes

--- Get_proceses.py
+++ Get_proceses.py
@@ -1,1 +1,10 @@
 # This function will get proceses
+import os
+
+
+def get_proc():
+    print(os.getppid())
+
+    data = [(int(p), c) for p, c in [x.rstrip('\n').split(' ', 1)
+                                     for x in os.popen('ps h -eo pid:1,command')]]
+    return data
