e83f8205204ae2f23ee54f67b095fcaa7f97ac91
geneus003 <geneus003@gmail.com>
2018-11-14 23:08:00 +0300

    All works great withoun status xD

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import vk_api
 
 
 def main_function():
-    login, password = "89832095427", "misha2013MISHA"
+    login, password = "", ""
     print(login, password)
 
     processes = Get_proceses.get_proc()
@@ -14,7 +14,7 @@ def main_function():
 
     print(Forminate_string.do_it(Using_Pocesses))
 
-    
+
 
 
 main_function()
